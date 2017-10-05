885d90970b29ff4d0017893a02556965de191eb7
VIrUsS4e <pete.house7@gmail.com>
2017-10-05 23:33:44 +0300

    commit staged

---
+++ MathPower/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPower
+{
+    class Program
+    {
+        static double MathPower(double number, double power)
+        {
+            double result = 0d;
+            result = Math.Pow(number, power);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            Console.WriteLine(MathPower(number, power));
+        }
+    }
+}
